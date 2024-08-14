599f3c6671cd2960198ca6ce8bcd40b3476cb3a6
svbjadhav <93444749+svbjadhav@users.noreply.github.com>
2024-08-14 13:26:50 +0530

    Update streamlit_app.py
    If fruit_chosen moved

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
@@ -26,7 +27,11 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    
+
+    for fruit_chosen in ingredients_list:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
@@ -45,9 +50,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# New section to display fruityvice nutrition information
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
