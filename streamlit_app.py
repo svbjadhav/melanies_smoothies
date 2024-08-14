f2c3c2d9c4fde939cafede358eb90053874cb9fb
svbjadhav <93444749+svbjadhav@users.noreply.github.com>
2024-08-14 13:34:21 +0530

    Update streamlit_app.py
    For loop fruits chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,6 @@ if ingredients_list:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
 
     # st.write(ingredients_string)
 
