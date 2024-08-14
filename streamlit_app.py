3792b3706066823669305f1fd08900087de0e4e3
svbjadhav <93444749+svbjadhav@users.noreply.github.com>
2024-08-14 13:32:24 +0530

    Update streamlit_app.py
    For loop chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,11 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-
+    
     for fruit_chosen in ingredients_list:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information'
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     for fruit_chosen in ingredients_list:
